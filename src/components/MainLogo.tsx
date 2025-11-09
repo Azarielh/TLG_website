537fa2ae3cd6a13a6159d6bdb517891f94da2a8a
Azarielh <33382899+Azarielh@users.noreply.github.com>
2025-11-09 01:32:44 +0000

    Commenter le conteneur du logo dans le composant MainLogo

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -2,8 +2,8 @@ import { createSignal } from "solid-js";
 
 export default function MainLogo() {
   return (
-    <div class="relative z-[70] min-h-[calc(100vh-56px)] flex items-start justify-center">
+    // <div class="relative z-[70] min-h-[calc(100vh-56px)] flex items-start justify-center">
       <img src="../../public/logo.png" alt="Logo TLG" class="h-100 w-auto object-contain" />
-    </div>
+    // </div>
   );
 }
