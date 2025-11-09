221996d2fba8f484c94bb88be9454c76441b5b3d
Azarielh <33382899+Azarielh@users.noreply.github.com>
2025-11-09 01:25:40 +0000

    Correction du chemin d'accès de l'image du logo dans le composant MainLogo

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -3,7 +3,7 @@ import { createSignal } from "solid-js";
 export default function MainLogo() {
   return (
     <div class="relative z-[70] min-h-[calc(100vh-56px)] flex items-start justify-center">
-      <img src="../../Assets/logo.png" alt="Logo TLG" class="h-100 w-auto object-contain" />
+      <img src="../../public/logo.png" alt="Logo TLG" class="h-100 w-auto object-contain" />
     </div>
   );
 }
