94d7600aa364eb71379891cb6352307c04c5013d
Azarielh <33382899+Azarielh@users.noreply.github.com>
2025-11-09 21:40:29 +0000

    Mettre à jour les liens et le contenu de la page "A propos" pour refléter les informations correctes et ajouter des sections sur l'esprit d'équipe, le compétitif et l'ambiance.

--- src/components/NavMain.tsx
+++ src/components/NavMain.tsx
@@ -7,7 +7,7 @@ export default function Nav() {
         <a href="/">The Legion</a>
         <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-2.5 items-center whitespace-nowrap">
           <SessionStatus text="Section à venir" placement="bottom">
-            <a class="text-[rgb(156,163,175)] px-3 py-2 rounded-[10px] transition-all duration-150 border border-transparent bg-transparent font-semibold hover:text-white hover:bg-[rgba(147,51,234,0.12)] hover:border-[rgba(168,85,247,0.18)]" href="/">A propos</a>
+            <a class="text-[rgb(156,163,175)] px-3 py-2 rounded-[10px] transition-all duration-150 border border-transparent bg-transparent font-semibold hover:text-white hover:bg-[rgba(147,51,234,0.12)] hover:border-[rgba(168,85,247,0.18)]" href="/about">A propos</a>
           </SessionStatus>
           <SessionStatus text="Section à venir" placement="bottom">
             <a class="text-[rgb(156,163,175)] px-3 py-2 rounded-[10px] transition-all duration-150 border border-transparent bg-transparent font-semibold hover:text-white hover:bg-[rgba(147,51,234,0.12)] hover:border-[rgba(168,85,247,0.18)]" href="/">Nos Jeux</a>
