db8b01edd08197b646e2e60b8dca3b4de938e20c
Azarielh <33382899+Azarielh@users.noreply.github.com>
2025-12-08 18:54:40 +0000

    Replace logo image format from PNG to SVG for improved scalability and quality

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -3,7 +3,7 @@ import { createSignal } from "solid-js";
 export default function MainLogo() {
   return (
     <div class="relative z-[70] flex items-center justify-center mt-2">
-      <img src="/logo.png" alt="Logo TLG" class="h-36 sm:h-44 md:h-48 w-auto object-contain" />
+      <img src="/logo.svg" alt="Logo TLG" class="h-36 sm:h-44 md:h-48 w-auto object-contain" />
     </div>
   );
 }
